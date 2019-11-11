18d8a338601116702bfe370f1c6e65b1a28e8fb2
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-11-11 13:22:02 -0500

    SuM

---
+++ src/sum.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+const MIN = (Number.MIN_SAFE_INTEGER)
+const MAX = (Number.MAX_SAFE_INTEGER)
+
+const amount = Number(readlineSync.question("\nLower bound: "));
+const amount2 = Number(readlineSync.question("Upper bound: "));
+const amount3 = Number(readlineSync.question("Lower bound: "));
+const amount4 = Number(readlineSync.question("Upper bound: "));
