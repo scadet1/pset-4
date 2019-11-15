1df924cf980bcae698f26ad088f222f6bdc1d8f6
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-11-15 13:20:35 -0500

    DO NOT TOUCH

---
+++ src/average.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+const amount = Number(readlineSync.question("\nHow's life: "));
+
+console.log ("\nBecause of Comp Sci?")
+
+const amount1 = Number(readlineSync.question("\n"));
+
+console.log ("\nSame")
